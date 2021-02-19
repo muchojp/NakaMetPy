443d622b07f31b674d48d1b64f8301b77b858244
Yuki Nakamura <1105mucho@gmail.com>
2021-02-19 11:54:00 +0900

    a little change of setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import setup
 install_requires = [
     'numpy',
     'matplotlib',
-    # 必要な依存ライブラリがあれば記述
 ]
 
 packages = [
@@ -11,7 +10,6 @@ packages = [
 ]
 
 console_scripts = [
-    # 特になし
     # 'sample_lib_cli=sample_lib_cli.call:main',
 ]
 
@@ -22,7 +20,7 @@ setup(
     version='0.2.0', # (2021.02.18)
     packages=packages,
     install_requires=install_requires,
-    url = 'https://hogehoge',
+    url = 'https://github.com/muchojp/NakaMetPy',
     author = 'Yuki Nakamura',
     author_email = 'contact.muchiwo@gmail.com',
     # entry_points={'console_scripts': console_scripts},
