6d242ae987c41a6783093b56d2a5cfbdcbf22b30
nakamura yuki <nakamura.yuki.867@s.kyushu-u.ac.jp>
2021-07-10 00:07:57 +0900

    docstringのimgのPATHの関係およびディレクトリ構成的にここに置くのがよさそう

--- src/nakametpy/cmaps.py
+++ src/nakametpy/cmaps.py
@@ -776,7 +776,7 @@ def get_colormap(name):
 def get_colormap_list():
     return cmap_names
 
-def _plot_each_colorbar(cmap_name, output='./img'):
+def _plot_each_colorbar(cmap_name, output='../../img'):
     import matplotlib.pyplot as plt
     import matplotlib as mpl
     import os
