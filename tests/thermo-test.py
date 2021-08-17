34f076c6d37423450d147eb09a43723c1d1ca155
nakamura yuki <nakamura.yuki.867@s.kyushu-u.ac.jp>
2021-08-17 21:13:26 +0900

    testを微調整

--- tests/thermo-test.py
+++ tests/thermo-test.py
@@ -1,5 +1,6 @@
 import unittest
-from src.nakametpy.thermo import potential_temperature # For Travis CI src.thermo is Right not ..src.thermo or .src.thermo
+from src.nakametpy.thermo import potential_temperature
+# For Travis CI src.thermo is Right not ..src.thermo or .src.thermo
 
 class ThermoTest(unittest.TestCase):
     def test_theta(self):
