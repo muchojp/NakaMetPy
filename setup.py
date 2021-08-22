0b58b55e2bbab7e6bd2ac73ae1c03309efb7dc1e
nakamura yuki <nakamura.yuki.867@s.kyushu-u.ac.jp>
2021-08-22 22:02:29 +0900

    2021.8.0へのアップデート

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ console_scripts = [
 setup(
     name='nakametpy',
     python_requires='>=3.6.0',
-    version='2021.7.0',
+    version='2021.8.0',
     description='Meteorological modules for calculation and colormap.',
     # packages=packages,
     packages=find_packages('src'),
