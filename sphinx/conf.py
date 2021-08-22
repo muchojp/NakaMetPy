7c5fb81730e908ca002f818e91209a22d4d2ed57
nakamura yuki <nakamura.yuki.867@s.kyushu-u.ac.jp>
2021-08-22 22:02:48 +0900

    2021.8.0へのドキュメンテーションのアップデート

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, muchojp'
 author = 'muchojp'
 
 # The full version, including alpha/beta/rc tags
-release = '2021.7.0'
+release = '2021.8.0'
 
 
 # -- General configuration ---------------------------------------------------
