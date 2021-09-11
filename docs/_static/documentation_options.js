57c7ffdc1757ec112c912a0e57f3b93cf8a70d9c
nakamura yuki <nakamura.yuki.867@s.kyushu-u.ac.jp>
2021-09-11 10:47:44 +0900

    Update for v2021.9.0

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2021.8.0',
+    VERSION: '2021.9.0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
