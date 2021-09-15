82c83529b40961d1fff033ce6df4e9b9b394bf0c
nakamura yuki <nakamura.yuki.867@s.kyushu-u.ac.jp>
2021-09-15 20:55:08 +0900

    change version for v2021.9.1

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, muchojp'
 author = 'muchojp'
 
 # The full version, including alpha/beta/rc tags
-release = '2021.9.0'
+release = '2021.9.1'
 
 
 # -- General configuration ---------------------------------------------------
