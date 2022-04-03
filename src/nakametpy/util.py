8471b7c416eeeb9922ab1a7fa635ac20760a279b
nakamura yuki <nakamura.yuki.867@s.kyushu-u.ac.jp>
2022-04-03 17:00:43 +0900

    Fix TYPO in docstring

--- src/nakametpy/util.py
+++ src/nakametpy/util.py
@@ -193,6 +193,7 @@ def anom_levels(levs):
   >>> levs = [0.5, 1., 2.]
   >>> print(anom_levels(levs))
   [-2.  -1.  -0.5  0.5  1.   2. ]
+  ```
   """
   levs = list(set(np.abs(levs)))
   levs.sort()
