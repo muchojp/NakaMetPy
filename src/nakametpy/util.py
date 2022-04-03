55acec9bf338cbad0fb6532d71410039bb129c2e
nakamura yuki <nakamura.yuki.867@s.kyushu-u.ac.jp>
2022-04-03 17:08:17 +0900

    change for sphinx code block

--- src/nakametpy/util.py
+++ src/nakametpy/util.py
@@ -192,9 +192,11 @@ def anom_levels(levs):
   .. highlight:: python
   .. code-block:: python
   
-  >>> levs = [0.5, 1., 2.]
-  >>> print(anom_levels(levs))
-  [-2.  -1.  -0.5  0.5  1.   2. ]
+  ```python
+    >>> levs = [0.5, 1., 2.]
+    >>> print(anom_levels(levs))
+    [-2.  -1.  -0.5  0.5  1.   2. ]
+  ```
   
   """
   levs = list(set(np.abs(levs)))
