cb2376b26cabb60f1828480c51856657a68840fc
muchojp <61620767+muchojp@users.noreply.github.com>
2024-03-22 23:53:18 +0000

    NMP#11AddTestCase 作業内容 - test_utilのメソッド名はtest_で始まるが必要があるため、メソッド名を修正

--- tests/test_util.py
+++ tests/test_util.py
@@ -2,7 +2,7 @@ import unittest
 from src.nakametpy.util import dt_yyyymmdd
 
 class UtilTest(unittest.TestCase):
-    def dt_yyyymmdd_001_test(self):
+    def test_dt_yyyymmdd_001(self):
         import datetime
         date = datetime.datetime(2024, 3, 23, 8, 15, 30)
         actual = dt_yyyymmdd(date)
