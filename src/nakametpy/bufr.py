d38b4470395506c6e6f3a0c69c7d20c9c610c3f5
Yuki Nakamura <1105mucho@gmail.com>
2024-10-23 07:06:24 +0900

    NMP#bufr 作業内容 Tableから情報を取得する関数の実装

--- src/nakametpy/bufr.py
+++ src/nakametpy/bufr.py
@@ -4,103 +4,126 @@
 
 import pandas as pd
 import re
+import os
 
 def parse_bufrtab(file_path: str) -> list:
-    with open(file_path, mode="r") as f:
-        raw_text = f.readlines()[1:-1]
-    # 特定の文字列から始まる行を除く
-    _records = list(filter(lambda x: not x.startswith("Table"), raw_text))
-    _records = list(filter(lambda x: not x.startswith("#"), _records))
-    _records = list(filter(lambda x: not x.startswith("END"), _records))
-    # 改行文字削除
-    _records = [_record.strip("\n") for _record in _records]
-    _records = [_record.strip() for _record in _records]
-    # 改行文字のみ
-    valid_records = list(filter(lambda x: x != "", _records))
-    return valid_records
+  with open(file_path, mode="r") as f:
+    raw_text = f.readlines()[1:-1]
+  # 特定の文字列から始まる行を除く
+  _records = list(filter(lambda x: not x.startswith("Table"), raw_text))
+  _records = list(filter(lambda x: not x.startswith("#"), _records))
+  _records = list(filter(lambda x: not x.startswith("END"), _records))
+  # 改行文字削除
+  _records = [_record.strip("\n") for _record in _records]
+  _records = [_record.strip() for _record in _records]
+  # 改行文字のみ
+  valid_records = list(filter(lambda x: x != "", _records))
+  return valid_records
 
 def parse_tableB_into_dataframe(version: str="STD_0_42") -> pd.DataFrame:
-    columns = ["F-XX-YYY", "SCALE", "REFERENCE_VALUE", "BIT_WIDTH", "UNIT", "MNEMONIC", "DESC_CODE", "ELEMENT_NAME"]
-    
-    bufrtab = f"./tables/bufrtab.TableB_{version:02}"
-    valid_records = parse_bufrtab(bufrtab)
-    # print(valid_records[-1])
-    df = pd.DataFrame([re.split("[|;]", irecord.strip()) for irecord in valid_records])
-    # 不要な文字を削除を削除
-    df = df.apply(lambda x: x.str.strip())
-    # Noneのみの列を削除
+  columns = ["F-XX-YYY", "SCALE", "REFERENCE_VALUE", "BIT_WIDTH", "UNIT", "MNEMONIC", "DESC_CODE", "ELEMENT_NAME"]
+  
+  bufrtab = os.path.join(os.path.dirname(__file__), f"./tables/bufrtab.TableB_{version:02}")
+  valid_records = parse_bufrtab(bufrtab)
+  # print(valid_records[-1])
+  df = pd.DataFrame([re.split("[|;]", irecord.strip()) for irecord in valid_records])
+  # 不要な文字を削除を削除
+  df = df.apply(lambda x: x.str.strip())
+  # ELEMENT_NAMEに;が含まれるカラムNoneのみの列を削除
+  if (len(columns) + 1) == len(df.columns.values):
+    # 最後の列のインデックスを取得
+    last_col = df.columns[-1]
+    df.loc[df[last_col].notnull(), df.columns[-2]] += ("; " + df[last_col])
+    # df[df.iloc[:, -1] is not None].iloc[:, -2] = df.iloc[:, :-2]+"; "+df.iloc[:, :-1]
     df = df.iloc[:, :-1]
-    df.columns = columns
-    return df
+  df.columns = columns
+  return df
 
 def parse_tableD_into_dict(version: str="STD_0_42") -> list:
-    bufrtab = f"./tables/bufrtab.TableD_{version:02}"
-    valid_records = parse_bufrtab(bufrtab)
-    data = dict()
-    fxxyyy = r"^\d-\d{2}-\d{3}" # F-XX-YYY
-    fxxyyy_notlast = r"^\d-\d{2}-\d{3} >" # F-XX-YYY >
-    for irecord in valid_records:
-        irec_list = [x.strip() for x in re.split("[|;]", irecord.strip())]
-        # 1レコードで比較
-        if re.match(fxxyyy, irecord):
-            ielm_list = []
-            iseq_list = irec_list
-        else:
-            jelm_list = irec_list
-            # 2列目のF-XX-YYYで比較
-            if re.match(fxxyyy_notlast, jelm_list[1]):
-                # " >"を削除
-                jelm_list[1] = jelm_list[1][:-2]
-                ielm_list.append(dict(FXXYYY=jelm_list[1], NAME=jelm_list[2]))
-            else:
-                ielm_list.append(dict(FXXYYY=jelm_list[1], NAME=jelm_list[2]))
-                data[iseq_list[0]] = dict(MNEMONIC=iseq_list[1], DCOD=iseq_list[2], NAME=iseq_list[3], SEQUENCE=ielm_list)
-    return data
+  bufrtab = os.path.join(os.path.dirname(__file__), f"./tables/bufrtab.TableD_{version:02}")
+  valid_records = parse_bufrtab(bufrtab)
+  data = dict()
+  fxxyyy = r"^\d-\d{2}-\d{3}" # F-XX-YYY
+  fxxyyy_notlast = r"^\d-\d{2}-\d{3} >" # F-XX-YYY >
+  for irecord in valid_records:
+    irec_list = [x.strip() for x in re.split("[|;]", irecord.strip())]
+    # 1レコードで比較
+    if re.match(fxxyyy, irecord):
+      ielm_list = []
+      iseq_list = irec_list
+    else:
+      jelm_list = irec_list
+      # 2列目のF-XX-YYYで比較
+      if re.match(fxxyyy_notlast, jelm_list[1]):
+        # " >"を削除
+        jelm_list[1] = jelm_list[1][:-2]
+        ielm_list.append(dict(FXXYYY=jelm_list[1], NAME=jelm_list[2]))
+      else:
+        ielm_list.append(dict(FXXYYY=jelm_list[1], NAME=jelm_list[2]))
+        data[iseq_list[0]] = dict(MNEMONIC=iseq_list[1], DCOD=iseq_list[2], NAME=iseq_list[3], SEQUENCE=ielm_list)
+  return data
 
 def parse_codeFlag_into_dict(version: str="STD_0_42") -> list:
-    bufrtab = f"./tables/bufrtab.CodeFlag_{version:02}"
-    valid_records = parse_bufrtab(bufrtab)
-    data = dict()
-    fxxyyy = r"^\d-\d{2}-\d{3}" # F-XX-YYY
-    valBit_notlast = r"^\d+ >" # \d+ >
-    for irecord in valid_records:
-        irec_list = [x.strip() for x in re.split("[|;]", irecord.strip())]
-        # 1レコードで比較
-        if re.match(fxxyyy, irecord):
-            ielm_list = []
-            dependency = None
-            iseq_list = irec_list
+  bufrtab = os.path.join(os.path.dirname(__file__), f"./tables/bufrtab.CodeFlag_{version:02}")
+  valid_records = parse_bufrtab(bufrtab)
+  data = dict()
+  fxxyyy = r"^\d-\d{2}-\d{3}" # F-XX-YYY
+  valBit_notlast = r"^\d+ >" # \d+ >
+  for irecord in valid_records:
+    irec_list = [x.strip() for x in re.split("[|;]", irecord.strip())]
+    # 1レコードで比較
+    if re.match(fxxyyy, irecord):
+      ielm_dict = dict()
+      dependency = None
+      # F-XX-YYY, MNEMONIC, CODEFLAG
+      # EXAMPLE: 0-01-003 | WMOR ; CODE
+      iseq_list = irec_list
+    else:
+      jelm_list = irec_list
+      # 2列目にDependencyがあるかを確認
+      if "=" in jelm_list[1]:
+        # "f-x1-yy1,f-x2-yy2=1,22,33"
+        dependency = jelm_list[1]
+      else:
+        #   | 0 > | Antarctica
+        if re.match(valBit_notlast, jelm_list[1]):
+          # " >"を削除
+          jelm_list[1] = jelm_list[1][:-2]
+        if dependency is None:
+          ielm_dict[jelm_list[1]] = jelm_list[2]
         else:
-            jelm_list = irec_list
-            # 2列目にDependencyがあるかを確認
-            if "=" in jelm_list[1]:
-                # "f-x1-yy1,f-x2-yy2=1,22,33"
-                dependency = jelm_list[1]
-            elif re.match(valBit_notlast, jelm_list[1]):
-                # " >"を削除
-                jelm_list[1] = jelm_list[1][:-2]
-                if dependency is None:
-                    ielm_list.append(dict(VALBIT=jelm_list[1], MEANING=jelm_list[2]))
-                else:
-                    ielm_list.append(dict(DEPENDENCY=dependency, VALBIT=jelm_list[1], MEANING=jelm_list[2]))
-            else:
-                if dependency is None:
-                    ielm_list.append(dict(VALBIT=jelm_list[1], MEANING=jelm_list[2]))
-                    data[iseq_list[0]] = dict(MNEMONIC=iseq_list[1], CODEFLAG=iseq_list[2], HAS_DEPENDENCY=False, VALBITS=ielm_list)
-                else:
-                    ielm_list.append(dict(DEPENDENCY=dependency, VALBIT=jelm_list[1], MEANING=jelm_list[2]))
-                    data[iseq_list[0]] = dict(MNEMONIC=iseq_list[1], CODEFLAG=iseq_list[2], HAS_DEPENDENCY=True, DEPENDENCY=dependency, VALBITS=ielm_list)
-    # print(data["0-01-034"])
-    return data
+          # print(type(dependency), dependency, jelm_list)
+          dependencies  = dependency.split("=")
+          _fxxyyy_list = dependencies[0].split(",")
+          # print(ielm_dict, type(ielm_dict), _fxxyyy_list[0], dependencies[1])
+          # EXAMPLE: (f-x1-yy1, f-x2-yy2)
+          for _fxxyyy in _fxxyyy_list:
+            if _fxxyyy not in ielm_dict.keys():
+              ielm_dict[_fxxyyy] = dict()
+            # EXAMPLE: "1,22,33"
+            for _idependency in dependencies[1].split(","):
+              if _idependency not in ielm_dict[_fxxyyy].keys():
+                ielm_dict[_fxxyyy][_idependency] = dict()
+              # 0-01-034 | GSES ; CODE
+              # ielm_dict["0-01-031"]["34"]["240"] = "Kiyose"
+              ielm_dict[_fxxyyy][_idependency][jelm_list[1]] = jelm_list[2]
+        if not re.match(valBit_notlast, jelm_list[1]):
+          if dependency is None:
+            data[iseq_list[0]] = dict(MNEMONIC=iseq_list[1], CODEFLAG=iseq_list[2], HAS_DEPENDENCY=False, VALBITS=ielm_dict)
+          else:
+            # print(dependency, ielm_dict)
+            data[iseq_list[0]] = dict(MNEMONIC=iseq_list[1], CODEFLAG=iseq_list[2], HAS_DEPENDENCY=True, DEPENDENCY=dependency, VALBITS=ielm_dict)
+  # print(data["0-01-034"])
+  return data
 
 
 if __name__=='__main__':
-    # df_b = parse_tableB_into_dataframe()
-    # print(df_b.tail())
-    # dict_d = parse_tableD_into_dict()
-    # print(dict_d["3-40-026"])
-    # dict_codeFlag = parse_codeFlag_into_dict()
-    # print(dict_codeFlag["0-01-033"])
-    # print(dict_codeFlag["0-01-034"])
-    dict_codeFlag = parse_codeFlag_into_dict("LOC_0_7_1")
-    print(dict_codeFlag)
+  df_b = parse_tableB_into_dataframe()
+  # print(df_b.head(50))
+  # dict_d = parse_tableD_into_dict()
+  # print(dict_d["3-40-026"])
+  # dict_codeFlag = parse_codeFlag_into_dict()
+  # print(dict_codeFlag["0-01-033"])
+  # print(dict_codeFlag["0-01-034"])
+  # dict_codeFlag = parse_codeFlag_into_dict("LOC_0_7_1")
+  # print(dict_codeFlag)
--- tests/test_bufr.py
+++ tests/test_bufr.py
@@ -17,12 +17,16 @@ class UtilTest(unittest.TestCase):
   def test_parse_tableB_into_dataframe_001(self):
     """
     test case: test_parse_tableB_into_dataframe_001
-    method:
+    
+    Method
+    --------
       parse_tableB_into_dataframe
-    args:
-      date: `datetime.datetime`
-    note:
-      opt の指定無し
+    Parameters
+    --------
+      version: `str`
+    Notes
+    --------
+      `version`の指定なし
     """
     # print(self.test_parse_tableB_into_dataframe_001.__doc__)
     actual = parse_tableB_into_dataframe()
@@ -32,18 +36,139 @@ class UtilTest(unittest.TestCase):
   def test_parse_tableB_into_dataframe_002(self):
     """
     test case: test_parse_tableB_into_dataframe_002
-    method:
+    
+    Method
+    --------
       parse_tableB_into_dataframe
-    args:
-      date: `datetime.datetime`
-    note:
-      opt の指定無し
+    Parameters
+    --------
+      version: `str`
+    Notes
+    --------
+      `version`の指定あり
     """
     # print(self.test_parse_tableB_into_dataframe_002.__doc__)
     for version, fxxyyy, expected in (("LOC_0_7_1", "0-01-195", "SACO"),
                                       ("STD_0_42", "0-01-024", "WSPDS")):
-      with self.subTest(version=version, fxxyyy=fxxyyy, expected=expected):
-        actual = parse_tableB_into_dataframe()
-        actual_value = actual[actual["F-XX-YYY"] == fxxyyy]["MNEMONIC"].values
-        self.assertEqual(actual_value, expected)
-        
+        with self.subTest(version=version, fxxyyy=fxxyyy, expected=expected):
+          actual = parse_tableB_into_dataframe(version)
+          # print(actual[actual["F-XX-YYY"] == fxxyyy])
+          actual_value = actual[actual["F-XX-YYY"] == fxxyyy]["MNEMONIC"].values
+          self.assertEqual(actual_value, expected)
+  
+  def test_parse_tableD_into_dict_001(self):
+    """
+    test case: test_parse_tableD_into_dict_001
+    
+    Method
+    --------
+      parse_tableD_into_dict
+    Parameters
+    --------
+      version: `str`
+    Notes
+    --------
+      `version`の指定なし
+    """
+    # print(self.test_parse_tableD_into_dict_001.__doc__)
+    actual = parse_tableD_into_dict()
+    self.assertEqual(actual["3-01-036"]["MNEMONIC"], "SHIPSEQ1")
+    self.assertEqual(actual["3-01-036"]["NAME"], "Ship")
+    self.assertEqual(actual["3-01-036"]["SEQUENCE"][2]["FXXYYY"], "3-01-011")
+    self.assertEqual(actual["3-01-036"]["SEQUENCE"][2]["NAME"], "Year, month, day")
+
+  def test_parse_tableD_into_dict_002(self):
+    """
+    test case: test_parse_tableD_into_dict_002
+    
+    Method
+    --------
+      parse_tableD_into_dict
+    Parameters
+    --------
+      version: `str`
+    Notes
+    --------
+      `version`の指定あり
+    """
+    # print(self.test_parse_tableD_into_dict_002.__doc__)
+    for version, fxxyyy1, mnemonic, name1, fxxyyy2, name2 in (
+      ("LOC_0_7_1", "3-52-003", "RCPTIM", "Report receipt time data", "0-04-204", "Receipt minute"),
+      ("STD_0_30", "3-16-007", "FRONTSEQ", "Front", "0-31-001", "Delayed descriptor replication factor")):
+      with self.subTest(version=version, fxxyyy1=fxxyyy1, mnemonic=mnemonic,
+                        name1=name1, fxxyyy2=fxxyyy2, name2=name2):
+        actual = parse_tableD_into_dict(version)
+        self.assertEqual(actual[fxxyyy1]["MNEMONIC"], mnemonic)
+        self.assertEqual(actual[fxxyyy1]["NAME"], name1)
+        self.assertEqual(actual[fxxyyy1]["SEQUENCE"][5]["FXXYYY"], fxxyyy2)
+        self.assertEqual(actual[fxxyyy1]["SEQUENCE"][5]["NAME"], name2)
+  
+  def test_parse_codeFlag_into_dict_001(self):
+    """
+    test case: test_parse_codeFlag_into_dict_001
+    
+    Method
+    --------
+      parse_codeFlag_into_dict
+    Parameters
+    --------
+      version: `str`
+    Notes
+    --------
+      `version`の指定なし
+    """
+    # print(self.test_parse_codeFlag_into_dict_001.__doc__)
+    actual = parse_codeFlag_into_dict()
+    # CODE, No dependency
+    self.assertEqual(actual["0-01-007"]["MNEMONIC"], "SAID")
+    self.assertEqual(actual["0-01-007"]["CODEFLAG"], "CODE")
+    self.assertEqual(actual["0-01-007"]["HAS_DEPENDENCY"], False)
+    self.assertEqual(actual["0-01-007"]["VALBITS"]["122"], "GCOM-W1")
+    # FLAG, No dependency
+    self.assertEqual(actual["0-02-002"]["MNEMONIC"], "TIWM")
+    self.assertEqual(actual["0-02-002"]["CODEFLAG"], "FLAG")
+    self.assertEqual(actual["0-02-002"]["HAS_DEPENDENCY"], False)
+    self.assertEqual(actual["0-02-002"]["VALBITS"]["3"], "Originally measured in km h**-1")
+    # CODE, has dependency
+    self.assertEqual(actual["0-01-034"]["MNEMONIC"], "GSES")
+    self.assertEqual(actual["0-01-034"]["CODEFLAG"], "CODE")
+    self.assertEqual(actual["0-01-034"]["HAS_DEPENDENCY"], True)
+    self.assertEqual(len(actual["0-01-034"]["VALBITS"].keys()), 3)
+    self.assertEqual(len(actual["0-01-034"]["VALBITS"]["0-01-031"]["34"].keys()), 4)
+    self.assertEqual(actual["0-01-034"]["VALBITS"]["0-01-031"]["34"]["240"], "Kiyose")
+    self.assertEqual(actual["0-01-034"]["VALBITS"]["0-01-035"]["46"]["0"], "No sub-centre")
+  
+  def test_parse_codeFlag_into_dict_002(self):
+    """
+    test case: test_parse_codeFlag_into_dict_002
+    
+    Method
+    --------
+      parse_codeFlag_into_dict
+    Parameters
+    --------
+      version: `str`
+    Notes
+    --------
+      `version`の指定なし
+    """
+    # print(self.test_parse_codeFlag_into_dict_002.__doc__)
+    actual = parse_codeFlag_into_dict("LOC_0_7_1")
+    # CODE, No dependency
+    self.assertEqual(actual["0-02-194"]["MNEMONIC"], "AUTO")
+    self.assertEqual(actual["0-02-194"]["CODEFLAG"], "CODE")
+    self.assertEqual(actual["0-02-194"]["HAS_DEPENDENCY"], False)
+    self.assertEqual(actual["0-02-194"]["VALBITS"]["3"], "METAR/SPECI report with 'A01' found in report, but 'AUTO' not found in report")
+    # FLAG, No dependency
+    self.assertEqual(actual["0-33-200"]["MNEMONIC"], "WSEQC1")
+    self.assertEqual(actual["0-33-200"]["CODEFLAG"], "FLAG")
+    self.assertEqual(actual["0-33-200"]["HAS_DEPENDENCY"], False)
+    self.assertEqual(actual["0-33-200"]["VALBITS"]["26"], "Rain flag based on TBs (or from SDR processor)")
+    # CODE, has dependency
+    self.assertEqual(actual["0-07-246"]["MNEMONIC"], "PQM")
+    self.assertEqual(actual["0-07-246"]["CODEFLAG"], "CODE")
+    self.assertEqual(actual["0-07-246"]["HAS_DEPENDENCY"], True)
+    self.assertEqual(len(actual["0-07-246"]["VALBITS"].keys()), 1)
+    self.assertEqual(len(actual["0-07-246"]["VALBITS"]["0-07-247"]["1"].keys()), 9)
+    self.assertEqual(actual["0-07-246"]["VALBITS"]["0-07-247"]["1"]["15"], "Observation is flagged for non-use by analysis")
+    self.assertEqual(actual["0-07-246"]["VALBITS"]["0-07-247"]["14"]["1"], "Good")
