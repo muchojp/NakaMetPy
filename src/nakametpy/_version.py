d347f1a3301e448220cff58d8e2fa87028006d99
Yuki Nakamura <1105mucho@gmail.com>
2025-09-19 20:10:40 +0900

    20250919 fix bug 2

--- src/nakametpy/_version.py
+++ src/nakametpy/_version.py
@@ -5,4 +5,4 @@
 
 
 def get_version():
-    return '2025.9.0'
+    return '2025.9.1'
--- src/nakametpy/bufr.py
+++ src/nakametpy/bufr.py
@@ -61,7 +61,7 @@ def parse_tableB_into_dataframe(version: str=f"STD_0_{LATEST_MASTER_TABLE_VERSIO
   """
   columns = ['F-XX-YYY', "SCALE", "REFERENCE_VALUE", "BIT_WIDTH", "UNIT", 'MNEMONIC', "DESC_CODE", 'ELEMENT_NAME']
   
-  bufrtab = os.path.join(os.path.dirname(__file__), f"./tables/bufrtab.TableB_{version}")
+  bufrtab = os.path.join(os.path.dirname(__file__), "tables", f"bufrtab.TableB_{version}")
   valid_records = parse_bufrtab(bufrtab)
   # print(valid_records[-1])
   df = pd.DataFrame([re.split("[|;]", irecord.strip()) for irecord in valid_records])
@@ -91,7 +91,7 @@ def parse_tableD_into_dict(version: str=f"STD_0_{LATEST_MASTER_TABLE_VERSION:02}
   -------
   `dict`: master table D
   """
-  bufrtab = os.path.join(os.path.dirname(__file__), f"./tables/bufrtab.TableD_{version}")
+  bufrtab = os.path.join(os.path.dirname(__file__), "tables", f"bufrtab.TableD_{version}")
   valid_records = parse_bufrtab(bufrtab)
   data = dict()
   fxxyyy = r"^\d-\d{2}-\d{3}" # F-XX-YYY
@@ -128,7 +128,7 @@ def parse_codeFlag_into_dict(version: str=f"STD_0_{LATEST_MASTER_TABLE_VERSION:0
   -------
   `dict`: master table CODE/FLAG
   """
-  bufrtab = os.path.join(os.path.dirname(__file__), f"./tables/bufrtab.CodeFlag_{version}")
+  bufrtab = os.path.join(os.path.dirname(__file__), "tables", f"bufrtab.CodeFlag_{version}")
   valid_records = parse_bufrtab(bufrtab)
   data = dict()
   fxxyyy = r"^\d-\d{2}-\d{3}" # F-XX-YYY
